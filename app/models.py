c5773d2d5dfb5f5421c601705d89af2afb86c63e
kikuoo <kikuoo505@gmail.com>
2025-03-11 14:51:30 +0900

    250311-12

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256))
+    password_hash = db.Column(db.String(512))
     is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
