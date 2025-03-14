3d4c1f80badacb03562b3519a62c3ade3d56dd3f
kikuoo <kikuoo505@gmail.com>
2025-03-14 21:42:46 +0900

    250314-12

--- wsgi.py
+++ wsgi.py
@@ -90,4 +90,8 @@ init_database()
 application = app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # ローカル開発環境用
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+else:
+    # Render環境用
+    app = application
